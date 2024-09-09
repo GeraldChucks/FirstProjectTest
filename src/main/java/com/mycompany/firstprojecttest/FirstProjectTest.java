b8fb31565cae6a08e5b0cb84784306c4e0bf63ae
RC_Student_lab <RC_Student_lab@10.0.1.11>
2024-09-09 07:29:00 +0000

    first commit

---
+++ src/main/java/com/mycompany/firstprojecttest/FirstProjectTest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.firstprojecttest;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class FirstProjectTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
